4fb6a98b1d561473aa8b383615f3398766c0efb1
Hamza Rahman <hamza.rahman2016@outlook.com>
2021-09-27 02:48:20 +0500

    MVC Basics, Views & Controllers

--- WebAppDemo2/Controllers/HomeController.cs
+++ WebAppDemo2/Controllers/HomeController.cs
@@ -12,5 +12,17 @@ namespace WebAppDemo2.Controllers
         {
             return View();
         }
+        public IActionResult About()
+        {
+            return View();
+        }
+        public IActionResult Contact()
+        {
+            return View();
+        }
+        public IActionResult Projects()
+        {
+            return View();
+        }
     }
 }
